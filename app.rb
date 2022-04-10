a5cb192e15fe1cef32d9a1a8b690d089a176dad3
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-04-10 10:35:35 +0100

    Console log when a game is loaded

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 require "require_all"
 
-set :port, 80
+set :port, 1393
 
 require_rel "models"
 
 get "/" do
   word = Word.new
   @word = word.newWord.upcase
-  @definition = define(@word)
+  @definition = "Offdline"#define(@word)
+  puts "[GAME] with #{word} : #{definition}"
   erb :main
 end
