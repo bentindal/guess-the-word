b9d94bb8c09ee70be332c7f567debb4ae03669b9
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-04-13 16:58:16 +0100

    Removed word class and replaced with a function

--- models/word.rb
+++ models/word.rb
@@ -1,7 +1,5 @@
-class Word
-  def newWord
-    listOfWords = File.read('words.txt').upcase.lines.map &:split
-    randomNumber = rand(1..listOfWords.length)
-    theWord = listOfWords[randomNumber].to_s[2..6]
-  end
+def newWord
+  listOfWords = File.read('words.txt').upcase.lines.map &:split
+  randomNumber = rand(1..listOfWords.length)
+  theWord = listOfWords[randomNumber].to_s[2..6]
 end
