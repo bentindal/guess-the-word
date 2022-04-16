88fcfadaffc7762028f4231dce8a306c8e3f3c40
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-04-16 14:53:05 +0100

    Only uses 1000 most common now

--- models/word.rb
+++ models/word.rb
@@ -1,5 +1,5 @@
 def newWord
   listOfWords = File.read('words.txt').upcase.lines.map &:split
-  randomNumber = rand(1..listOfWords.length)
+  randomNumber = rand(1..1000) # 1000 most common words
   theWord = listOfWords[randomNumber].to_s[2..6]
 end
