63105244088f5d4f709c7bb9e10c0c0db52293f0
Benjamin Tindal <btindal1@sheffield.ac.uk>
2022-05-02 14:14:43 +0100

    Removed copy to clipboard button

--- public/scripts/customGame.js
+++ public/scripts/customGame.js
@@ -1,18 +1,3 @@
-function copyToClipboard() {
-    /* Get the text field */
-    var copyText = document.getElementById("customLink");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.innerText);
-  
-    /* Alert the copied text */
-    alert("Link Copied to Clipboard");
-  }
-
 function updateLink(){
   console.log('updating link')
   var linkElement = document.getElementById("customLink")
@@ -20,5 +5,4 @@ function updateLink(){
     linkElement.innerText = "www.btindal.uk/custom-game/"+cID
     linkElement.setAttribute("href", "custom-game/"+cID)
   }
-  copyToClipboard()
 }
